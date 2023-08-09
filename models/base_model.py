8af3099f2b955e66c3fada7b621a141f9f8a8ece
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-08-09 11:25:06 -0700

    first two tasks

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+import uuid
+"""Defines the base model for the project."""
+
+
+class BaseModel():
+    """
+    BaseModel that defines all common
+    attributes/methods for other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """initiates new instance"""
+        dt_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) is not 0:
+            for k, v in kwargs.items():
+                if k is not "__class__":
+                    if k == "created_at" or k == "updated_at":
+                        setattr(self, k, datetime.strptime(v, dt_format))
+                    else:
+                        setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def save(self):
+        """updates the public instance attribute
+        updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+           returns a dictionary containing all keys/values
+           of __dict__ of the instance with __class__ key that have
+           class name as value and format the created and updated at keys
+        """
+        new_dict = {**self.__dict__, "__class__": "BaseModel"}
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
+
+    def __str__(self):
+        """prints the string represntation of an object"""
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
