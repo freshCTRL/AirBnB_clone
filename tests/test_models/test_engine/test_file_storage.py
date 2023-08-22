0e19d3aca866a12d5c603074f68f27bed384ec92
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-22 14:01:14 +0100

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 import unittest
 from models.base_model import BaseModel
 from models import storage
+import datetime
 
 
 class TestFileStorage(unittest.TestCase):
@@ -19,9 +20,19 @@ class TestFileStorage(unittest.TestCase):
         save_my_created_at = sample.created_at
         sample.save()
         k = storage.all()
+
+        def gt(dt_str):
+            """
+                reversing an isofomarted date
+            """
+            dt, _, us = dt_str.partition(".")
+            dt = datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
+            us = int(us.rstrip("Z"), 10)
+            return dt + datetime.timedelta(microseconds=us)
+
         for key0 in k.keys():
             if k[key0].to_dict()["id"] == saved_id:
-                self.assertEqual(k[key0].to_dict()["created_at"], save_my_created_at)
+                self.assertEqual(gt(k[key0].to_dict()["created_at"]), save_my_created_at)
 
         content = storage.reload()
         self.assertFalse(content == {})
