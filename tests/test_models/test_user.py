8e34ebe650affa12b3bc0d2c34eddf75a5ec42c9
freshCTRL <111054651+freshCTRL@users.noreply.github.com>
2023-08-22 13:38:08 +0100

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,28 +10,15 @@ class TestUser(unittest.TestCase):
     """
         test for class (base_models)
     """
-
-    def setUp(self):
-        """
-            initialising a test...
-        """
-        pass
-
-    def tearDown(self):
-        """
-            closing a test...
-        """
-        pass
-
     def test_initialisation_str_save(self):
         """
             test for (init), (str), (save) method
         """
         sample1 = User()
-        self.assertIsNone(sample1.first_name)
-        self.assertIsNone(sample1.last_name)
-        self.assertIsNone(sample1.email)
-        self.assertIsNone(sample1.password)
+        self.assertIsNotNone(sample1.first_name)
+        self.assertIsNotNone(sample1.last_name)
+        self.assertIsNotNone(sample1.email)
+        self.assertIsNotNone(sample1.password)
         self.assertIsNotNone(sample1.id)
         self.assertIsNotNone(sample1.created_at)
         self.assertIsNotNone(sample1.updated_at)
